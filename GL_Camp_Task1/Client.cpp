4ef8c77f29b3f56f5df9a095f54300098a188f22
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2023-09-13 16:46:11 +0300

    Server gets message from client and launches the app using CreateProcess()

--- GL_Camp_Task1/Client.cpp
+++ GL_Camp_Task1/Client.cpp
@@ -2,9 +2,8 @@
 #include <cstring>
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <winsock2.h>
-#include <ws2tcpip.h>
 
-int main() {
+int main() {    
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "Error: Unable to initialize Winsock." << std::endl;
@@ -18,11 +17,10 @@ int main() {
         return 1;
     }
 
+
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(8888); // Port number
-    //unsigned char buf[sizeof(struct in6_addr)];
-    //serverAddr.sin_addr.s_addr = inet_pton(AF_INET, "127.0.0.1", buf);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Use localhost
 
     if (connect(clientSocket, reinterpret_cast<struct sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
@@ -32,23 +30,51 @@ int main() {
         return 1;
     }
 
-    char buffer[1024];
-    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesRead == SOCKET_ERROR) {
-        std::cerr << "Error: Failed to receive data from the server." << std::endl;
+    const char* applicationName = "notepad.exe"; // Replace with the desired application name
+
+    // Send the application name to the server
+    if (send(clientSocket, applicationName, strlen(applicationName), 0) == SOCKET_ERROR) {
+        std::cerr << "Error: Failed to send the application name to the server." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
     }
-    else if (bytesRead == 0) {
-        std::cerr << "Connection closed by the server." << std::endl;
+
+    char response[1024];
+    int bytesRead = recv(clientSocket, response, sizeof(response), 0);
+    if (bytesRead == SOCKET_ERROR) {
+        std::cerr << "Error: Failed to receive a response from the server." << std::endl;
     }
     else {
-        buffer[bytesRead] = '\0'; // Null-terminate the received data
-        std::cout << "Received: " << buffer << std::endl;
+        response[bytesRead] = '\0';
+        std::cout << "Server response: " << response << std::endl;
+    }
+
+    while (true) {
+        char status[1024];
+        bytesRead = recv(clientSocket, status, sizeof(status), 0);
+        if (bytesRead == SOCKET_ERROR) {
+            std::cerr << "Error: Failed to receive application status from the server." << std::endl;
+            break;
+        }
+        else if (bytesRead == 0) {
+            std::cerr << "Server closed the connection." << std::endl;
+            break;
+        }
+        else {
+            status[bytesRead] = '\0';
+            std::cout << "Application status: " << status << std::endl;
+
+            // Check if the application has stopped
+            if (strcmp(status, "Application Stopped") == 0) {
+                std::cout << "The application has stopped." << std::endl;
+                break; // Exit the loop
+            }
+        }
     }
 
     closesocket(clientSocket);
     WSACleanup();
 
-    getchar();
-    
     return 0;
 }
