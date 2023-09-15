5a8b79a0d7c5f41e6e7f410c0b14de2163728ffb
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2023-09-15 12:20:34 +0300

    Done an IPEndpoin class It uses inet_pton inside and stores the hostname, ip_addr, port and individual bytes of address.

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -9,6 +9,25 @@ int main()
 	
 	if (Network::Initialize()) {
 		std::cout << "Winsock API succesfully itialized." << std::endl;
+
+		IPEndpoint test("127.0.0.1", 8080);
+		if (test.GetIPVersion() == IPVersion::IPv4)
+		{
+			std::cout 
+				<< "Hostname: " << test.GetHostName() << "\n"
+				<< "IP: " << test.GetIpString() << "\n"
+				<< "Port: " << test.GetPort() << "\n"
+				<< "IP Bytes..." << std::endl;
+			for (auto& digit : test.GetIPBytes())
+			{
+				std::cout << (int)digit << std::endl;
+			}
+		}
+		else
+		{
+			std::cerr << "This is not a valid ipv4 addresss." << std::endl;
+		}
+
 		Socket socket;
 		if (socket.Create() == PResult::P_Success)
 		{
