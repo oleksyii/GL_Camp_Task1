66a39a5044613ff28b93b2e1adc77bd3c5bbd99b
Alex <garom.gift@gmail.com>
2023-09-22 17:52:16 +0300

    Some more reorganization

--- build/CMakeFiles/Client.dir/Platform/Windows_.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Client.dir/Platform/Windows_.cpp.o: \
- /home/oleksii/GLTask1/src/Platform/Windows_.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Server.dir/Platform/Windows_.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Server.dir/Platform/Windows_.cpp.o: \
- /home/oleksii/GLTask1/src/Platform/Windows_.cpp \
- /usr/include/stdc-predef.h
